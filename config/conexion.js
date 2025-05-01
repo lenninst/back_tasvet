17219b898a14478a8bfb8630b94aec7a91300e67
lenninst <leninguaminga@gmail.com>
2025-05-01 03:01:09 -0500

    config: update enviroment variables

--- config/conexion.js
+++ config/conexion.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: 'localhost',  
-  user: 'root',       
-  password: '',       
-  database: 'tasvet_operativo' 
+  user: 'lennuser',       
+  password: 'batman',       
+  database: 'tasvet' 
 });
 
 db.connect(err => {
--- config/key.js
+++ config/key.js
@@ -1,3 +1,4 @@
 module.exports = {
+/* HACK:  xd,:) */ 
     JWT_SECRET: 'UjXn2r5u8x/A?D(G+KaPdSgVkYp3s6v9y$B&E)H@McQeThWmZq4t7w!z%C*F-JaN',
 }
